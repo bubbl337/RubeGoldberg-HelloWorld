2b92f0254b7253feed9ebfed6784dd6e74d86359
Obscure-Git <Obscure-Git@users.noreply.github.com>
2021-02-21 11:00:22 +0545

    made more confusing

--- src/python/main.py
+++ src/python/main.py
@@ -1,11 +1,13 @@
 import writer
 
-def did_i_get_lana_pregnant():
-    f=open('text/text.txt','r')
-    answer_to_my_question=f.readlines()
-    return answer_to_my_question
 
-my_question=did_i_get_lana_pregnant()
+def getHelloWorld():
+    f = open("./text.txt", "r")
+    output = f.readlines()
+    return output
+
+
+my_question = getHelloWorld()
 
 for checking_my_answer in my_question:
     print(checking_my_answer)
--- src/python/writer.py
+++ src/python/writer.py
@@ -1,7 +1,34 @@
-def txt_writer(write):
-    f=open('text/text.txt','w')
-    f.writelines(write)
+import random
+
+
+def txt_writer(words):
+    f = open("text.txt", "w")
+    f.writelines(words)
     f.close()
 
-write=['H','E','L','L','O',' ','W','O','R','L','D','!','!','!']
-txt_writer(write)
+
+# ? All the letters of the word - "Hello World!!!"
+letters = ["H", "E", "L", "O", "W", "R", "D", "!", " "]
+
+word = (
+    letters[0]
+    + letters[1]
+    + letters[2]
+    + letters[2]
+    + letters[3]
+    + letters[8]
+    + letters[4]
+    + letters[3]
+    + letters[5]
+    + letters[2]
+    + letters[6]
+    + letters[7]
+    + letters[7]
+    + letters[7]
+)
+
+randomise = [word.lower(), word.upper(), word.title(), word.capitalize()]
+
+words = random.choice(randomise)
+
+txt_writer(words)
